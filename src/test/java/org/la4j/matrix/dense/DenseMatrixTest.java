daf291e0e8aeca62804066acbcb591d8c315af40
Jakob Moellers <jakob.moellers@gmail.com>
2013-06-24 10:21:29 +0200

    Use Matrix.equal() and predefined correct matrix for testing on correctness

--- src/test/java/org/la4j/matrix/dense/DenseMatrixTest.java
+++ src/test/java/org/la4j/matrix/dense/DenseMatrixTest.java
@@ -48,12 +48,17 @@ public abstract class DenseMatrixTest extends AbstractMatrixTest {
 				{ 1.0, 2.0 }, 
 				{ 3.0, 4.0 } 
 		};
-		Basic2DMatrix m = new Basic2DMatrix(array);
-		Basic2DMatrix n = (Basic2DMatrix) m.rotate();
-		assertTrue(n.get(0, 0) == 3.0);
-		assertTrue(n.get(0, 1) == 1.0);
-		assertTrue(n.get(1, 0) == 4.0);
-		assertTrue(n.get(1, 1) == 2.0);
+		
+		double array2[][] = new double[][] { 
+				{ 3.0, 1.0 }, 
+				{ 4.0, 2.0 } 
+		};
+		Basic2DMatrix m1 = new Basic2DMatrix(array);
+		Basic2DMatrix m2 = (Basic2DMatrix) m1.rotate();
+		Basic2DMatrix m3 = new Basic2DMatrix(array2);
+		
+		assertTrue(m2.equals(m3));
+		
 	}
 
 }
