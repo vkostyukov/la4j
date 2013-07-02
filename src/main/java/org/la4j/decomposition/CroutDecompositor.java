d0a7fbbe2784932fb8d1e70e6b2a227968e19152
root <root@debian>
2013-07-02 04:53:43 +0300

    replaced tabs with spaces un CroutDecompositor.java

--- src/main/java/org/la4j/decomposition/CroutDecompositor.java
+++ src/main/java/org/la4j/decomposition/CroutDecompositor.java
@@ -23,8 +23,8 @@ public class CroutDecompositor implements MatrixDecompositor {
      * @param factory
      * @return { L, U }
      */
-	@Override
-	public Matrix[] decompose(Matrix matrix, Factory factory) {
+    @Override
+    public Matrix[] decompose(Matrix matrix, Factory factory) {
 
         if (matrix.rows() != matrix.columns()) {
             throw new IllegalArgumentException("Wrong matrix size: " 
@@ -34,27 +34,27 @@ public class CroutDecompositor implements MatrixDecompositor {
         Matrix l = factory.createMatrix(matrix.rows(), matrix.columns());
         Matrix u = factory.createIdentityMatrix(matrix.rows());
         for (int j = 0; j < l.columns(); j++){
-        	for (int i = j; i < l.rows(); i++){
-        		double s = 0.0;
-        		for (int k = 0; k < j; k++){
-        			s = s + l.get(i, k)*u.get(k, j);
-        		}
-        		l.set(i,j,matrix.get(i, j) - s);
-        	}
-        		
-        	for (int i = j; i < l.rows(); i++){
-        		double s = 0.0;
-        		for (int k = 0; k < j; k++){
-        			s = s + l.get(j, k)*u.get(k, i);
-        		}
-        		if (Math.abs(l.get(j, j)) < Matrices.EPS){
-        			throw new IllegalArgumentException("Singular matrix!");
-        		}
-	        		u.set(j,i,(matrix.get(j, i) - s)/l.get(j, j));
-        		}        		        	
+            for (int i = j; i < l.rows(); i++){
+                double s = 0.0;
+                for (int k = 0; k < j; k++){
+                    s = s + l.get(i, k)*u.get(k, j);
+                }
+                l.set(i,j,matrix.get(i, j) - s);
+            }
+            
+            for (int i = j; i < l.rows(); i++){
+                double s = 0.0;
+                for (int k = 0; k < j; k++){
+                    s = s + l.get(j, k)*u.get(k, i);
+                }
+                if (Math.abs(l.get(j, j)) < Matrices.EPS){
+                    throw new IllegalArgumentException("Singular matrix!");
+                }
+                    u.set(j,i,(matrix.get(j, i) - s)/l.get(j, j));
+                }        		        	
         }
   
         return new Matrix[] { l, u };
-	}		
+    }		
 
 }
