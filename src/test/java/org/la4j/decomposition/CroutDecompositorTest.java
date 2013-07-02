f99d8d7ba68578c5ee359788e4ff82a9b64aa5ca
root <root@debian>
2013-07-02 03:38:04 +0300

    replaced tabs with spaces in CroutDecompositorTest.class

--- src/test/java/org/la4j/decomposition/CroutDecompositorTest.java
+++ src/test/java/org/la4j/decomposition/CroutDecompositorTest.java
@@ -5,38 +5,38 @@ import junit.framework.TestSuite;
 
 public class CroutDecompositorTest extends AbstractDecompositorTest {
 
-	@Override
+    @Override
 	public MatrixDecompositor decompositor() {
-		return new CroutDecompositor();
-	}
+	    return new CroutDecompositor();
+    }
 
-	@Override
-	public double[][] input() {
-		return new double[][] {
+    @Override
+    public double[][] input() {
+        return new double[][] {
                 { 1.0, 0.0, 2.0 }, 
                 { 0.0, 10.0, 0.0 }, 
                 { 2.0, 0.0, 9.0 } 
         };
-	}
+    }
 
-	@Override
-	public double[][][] output() {
-		return new double[][][] {
+    @Override
+    public double[][][] output() {
+        return new double[][][] {
                 { 
                     { 1.0, 0.0, 0.0 },
                     { 0.0, 10.0, 0.0},
                     { 2.0, 0.0, 5.0 } 
                 },
                 { 
+
                     { 1.0, 0.0, 2.0 }, 
                     { 0.0, 1.0, 0.0 }, 
                     { 0.0, 0.0, 1.0 } 
                 } 
         };
-	}
-	
-	public static Test suite() {
-		return new TestSuite(CroutDecompositorTest.class);
-	}
+    }
 
+    public static Test suite() {
+        return new TestSuite(CroutDecompositorTest.class);
+    }
 }
