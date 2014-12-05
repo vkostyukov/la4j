ab3953371475f676779eee22cec91fbc8d4de605
SamoylovMD <samoylov.md@gmail.com>
2014-12-05 08:27:36 +0300

    Decomposer tests refactored, errors not confirmed

--- src/test/java/org/la4j/decomposition/CholeskyDecompositorTest.java
+++ src/test/java/org/la4j/decomposition/CholeskyDecompositorTest.java
@@ -31,4 +31,89 @@ public class CholeskyDecompositorTest extends AbstractDecompositorTest {
         return LinearAlgebra.CHOLESKY;
     }
 
+    @Test
+    public void testDecompose_1x1() {
+        double[][] input = new double[][]{
+                {56}
+        };
+        double[][][] output = new double[][][]{
+                {
+                        {7.483}
+                }
+        };
+        performTest(input, output);
+    }
+
+    @Test
+    public void testDecompose_2x2() {
+        double[][] input = new double[][]{
+                {1.0, -2.0},
+                {-2.0, 5.0}
+        };
+        double[][][] output = new double[][][]{
+                {
+                        {1.0, 0.0},
+                        {-2.0, 1.0}
+                }
+        };
+        performTest(input, output);
+    }
+
+    @Test
+    public void testDecompose_3x3() {
+        double[][] input = new double[][]{
+                {1.0, -2.0, 0.0},
+                {-2.0, 5.0, 2.0},
+                {0.0, 2.0, 5.0}
+        };
+        double[][][] output = new double[][][]{
+                {
+                        {1.0, 0.0, 0.0},
+                        {-2.0, 1.0, 0.0},
+                        {0.0, 2.0, 1.0}
+                }
+        };
+        performTest(input, output);
+    }
+
+    @Test
+    public void testDecompose_4x4() {
+        double[][] input = new double[][]{
+                {18.0, 22.0, 54.0, 42.0},
+                {22.0, 70.0, 86.0, 62.0},
+                {54.0, 86.0, 174.0, 134.0},
+                {42.0, 62.0, 134.0, 106.0}
+        };
+        double[][][] output = new double[][][]{
+                {
+                        {4.243, 0.000, 0.000, 0.000},
+                        {5.185, 6.566, 0.000, 0.000},
+                        {12.728, 3.046, 1.650, 0.000},
+                        {9.899, 1.625, 1.850, 1.393}
+                }
+        };
+        performTest(input, output);
+    }
+
+    @Test
+    public void testDecompose_5x5() {
+        double[][] input = new double[][]{
+                {6.0, 15.0, 55.0, 72.0, 101.0},
+                {15.0, 55.0, 225.0, 229.0, 256.0},
+                {55.0, 225.0, 979.0, 1024.0, 1200.0},
+                {72.0, 229.0, 1024.0, 2048.0, 2057.0},
+                {101.0, 256.0, 1200.0, 2057.0, 6000.0}
+        };
+        double[][][] output = new double[][][]{
+                {
+                        {2.449, 0.000, 0.000, 0.000, 0.000},
+                        {6.124, 4.183, 0.000, 0.000, 0.000},
+                        {22.454, 20.917, 6.110, 0.000, 0.000},
+                        {29.394, 11.713, 19.476, 25.836, 0.000},
+                        {41.233, 0.837, 42.007, 0.661, 50.340}
+                }
+        };
+        performTest(input, output);
+    }
+
 }
